b52239f28860aa0e8ce8f92c2371b58283574d3f
antonio <antonio@pykiss.com>
2018-09-17 12:21:53 +0200

    .

--- package.js
+++ package.js
@@ -1,12 +1,11 @@
 /* global Package */
 
 Package.describe({
-  name: 'ocs:login',
-  version: '0.0.3',
-  // Brief, one-line summary of the package.
-  summary: '',
+  name: 'hacknlove:login',
+  version: '0.0.4',
+  summary: 'Para no repetir el mismo código cada vez que hago una webapp',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'git@hacknlove.github.com:hacknlove/meteor-login.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
